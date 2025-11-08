632746bc02c9ae614d5aaa3a90d822fbb54e3d74
Phillip Sievers <phillip.sievers@gmx.de>
2025-11-07 19:42:01 -0500

    linting and workspace

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,29 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+    ...nextVitals,
+    ...nextTs,
+    {
+        files: ["./**/*.{js,jsx,ts,tsx}"],
+        rules: {
+            indent: ["error", 4, { SwitchCase: 1 }],
+            quotes: ["error", "double"],
+            semi: ["error", "always"],
+        },
+    },
+    {
+        ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
+    },
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts",
+    ]),
 ]);
 
 export default eslintConfig;
